ddd844f75400eca7180265ebc5b3452680c85e5a
2sivasankar <sivasankarthikka@gmail.com>
2025-05-14 20:09:00 +0530

    javascript

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function myfunction(v) {
     document.getElementsByClassName("display-output")[0].value += v;
 }
+const btn=document.querySelector("button");
+function random(number){
+    return Math.floor(Math.random()*(number+1));
+
+}
+btn.addEventListener("click",()=>{
+    const color=`rgb(${random(255)},${random(255)},${random(255)})`;
+    document.body.style.backgroundColor=color;
+
+});
